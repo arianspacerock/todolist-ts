fdac3d58cc623a2caf43e13ff73b6c1400384930
arianspacerock <siropemptyspace@gmail.com>
2023-02-08 18:03:35 +0300

    init: start markdown a proj

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,33 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      <Todolist />
-    </div>
-  );
+    return (
+        <div>
+            <Todolist/>
+        </div>
+    );
 }
 
 const Todolist = () => {
-  return (
-      <div>TODOLIST</div>
-  )
+    return (
+        <div>
+            <h3>What to learn</h3>
+            <div>
+                <input type="text"/>
+                <button>+</button>
+                <ul>
+                    <li><input type="checkbox" checked={true}/><span>CSS&HTML</span></li>
+                    <li><input type="checkbox" checked={true}/><span>JS</span></li>
+                    <li><input type="checkbox" checked={false}/><span>React</span></li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App;
