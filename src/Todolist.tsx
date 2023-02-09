1a2c9a73244928786cf588d0afa409b2bf3794d9
arianspacerock <siropemptyspace@gmail.com>
2023-02-09 07:02:33 +0300

    ref: create component Todolist

---
+++ src/Todolist.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+type PropsType =  {
+    title: string
+}
+
+export const Todolist = (props: PropsType) => {
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input type="text"/>
+                <button>+</button>
+                <ul>
+                    <li><input type="checkbox" checked={true}/><span>CSS&HTML</span></li>
+                    <li><input type="checkbox" checked={true}/><span>JS</span></li>
+                    <li><input type="checkbox" checked={false}/><span>React</span></li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
+        </div>
+    )
+}
