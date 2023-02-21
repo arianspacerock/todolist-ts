cfd57e4a30ccd1a5d18550ec3ffd166b0129a41a
arianspacerock <siropemptyspace@gmail.com>
2023-02-21 06:58:45 +0300

    ref: add types for filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,36 @@
 import React, {useState} from 'react';
 
 import './App.css';
-import {TaskType, Todolist} from "./Todolist";
+import {Todolist} from "./Todolist";
+
+export type FilterValuesType = "all" | "completed" | "active"
 
 function App() {
 
-    const initTasks: Array<TaskType> = [
+    let [tasks, setTasks] = useState([
         {id: 1, title: "CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "React", isDone: false},
         {id: 4, title: "RTK", isDone: false},
-    ]
-
-    let arr = useState(initTasks)
-
-    let tasks = arr[0]
-    let setTasks = arr[1]
+    ])
+    let [filter, setFilter] = useState<FilterValuesType>("all")
     const removeTask = (id: number) => {
         let resultTasks = tasks.filter(t => t.id !== id)
+    }
+
+    let tasksForToDoList = tasks
+    if (filter === "completed") {
+        tasksForToDoList = tasks.filter( t => t.isDone === true)
+    }
+    if (filter === "active") {
+        tasksForToDoList = tasks.filter( t => t.isDone === false)
     }
 
     return (
         <div className="App">
             <Todolist
                 title="What to learn"
-                tasks={tasks}
+                tasks={tasksForToDoList}
                 removeTask={removeTask}
             />
         </div>
