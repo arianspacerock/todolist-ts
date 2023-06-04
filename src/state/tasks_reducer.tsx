8453fe7c40bb195df9a824042b1399c3b71aeea3
arianspacerock <siropemptyspace@gmail.com>
2023-06-04 16:43:15 +0300

    ref: start add reducers & tests

--- src/state/tasks_reducer.tsx
+++ src/state/tasks_reducer.tsx
@@ -1,4 +1,5 @@
 import {AssocTaskType} from "../App";
+import {v1} from "uuid";
 
 export const tasksReducer = (state: AssocTaskType, action: TodolistReducerActionType) : AssocTaskType => {
     switch (action.type) {
@@ -8,15 +9,24 @@ export const tasksReducer = (state: AssocTaskType, action: TodolistReducerAction
                 [action.payload.todolistId]: state[action.payload.todolistId].filter(t => t.id !== action.payload.taskId)
             }
         }
-        case "": {
-            return state
+        case "ADD-TASK": {
+            return {
+                ...state,
+                [action.payload.todolistId]: [{id: v1(), title: action.payload.title, isDone: false}, ...state[action.payload.todolistId]]
+            }
+        }
+        case "CHANGE-TASK-STATUS": {
+            return {
+                ...state,
+                [action.payload.todolistId]: state[action.payload.todolistId].map(t => t.id === action.payload.taskId ? {...t, isDone: action.payload.isDone} : t)
+            }
         }
         default:
             return state
     }
 }
 
-type TodolistReducerActionType = RemoveTaskActionType | SActionType
+type TodolistReducerActionType = RemoveTaskActionType | AddTaskActionType | ChangeTaskStatusActionType
 
 type RemoveTaskActionType = ReturnType<typeof removeTaskAC>
 export const removeTaskAC = (taskId: string, todolistId: string) => {
@@ -29,9 +39,25 @@ export const removeTaskAC = (taskId: string, todolistId: string) => {
     } as const
 }
 
-type SActionType = ReturnType<typeof secondAC>
-export const secondAC = (title: string, newTodolistId: string) => {
+type AddTaskActionType = ReturnType<typeof addTaskAC>
+export const addTaskAC = (title: string, newTodolistId: string) => {
     return {
-        type: "",
+        type: 'ADD-TASK',
+        payload: {
+            title: title,
+            todolistId: newTodolistId
+        }
+    } as const
+}
+
+type ChangeTaskStatusActionType = ReturnType<typeof changeTaskStatusAC>
+export const changeTaskStatusAC = (taskId: string, isDone: boolean, todolistId: string) => {
+    return {
+        type: 'CHANGE-TASK-STATUS',
+        payload: {
+            taskId: taskId,
+            isDone: isDone,
+            todolistId: todolistId
+        }
     } as const
 }
--- src/state/tasks_reduser.test.tsx
+++ src/state/tasks_reduser.test.tsx
@@ -1,4 +1,4 @@
-import {removeTaskAC, tasksReducer} from "./tasks_reducer";
+import {addTaskAC, changeTaskStatusAC, removeTaskAC, tasksReducer} from "./tasks_reducer";
 import {AssocTaskType} from "../App";
 import {v1} from "uuid";
 
@@ -48,13 +48,35 @@ test ('correct task should be added to correct array', () => {
         ]
     }
 
-    const action = removeTaskAC("TS2", "todolistID2");
+    const action = addTaskAC("TS2", "todolistID2");
 
     const endState = tasksReducer(startState, action)
 
     expect(endState["todolistID1"].length).toBe(3);
     expect(endState["todolistID2"].length).toBe(4);
     expect(endState["todolistID2"][0].id).toBeDefined();
-    expect(endState["todolistID2"][0].title).toBe("HTML&CSS2");
-    expect(endState["todolistID2"][0].isDone).toBe(true);
+    expect(endState["todolistID2"][0].title).toBe("TS2");
+    expect(endState["todolistID2"][0].isDone).toBe(false);
+})
+
+test('status of specified task should be changed', () => {
+    const startState: AssocTaskType = {
+        "todolistID1": [
+            {id: "1", title: "HTML&CSS", isDone: true},
+            {id: "2", title: "JS", isDone: true},
+            {id: "3", title: "ReactJS", isDone: false},
+        ],
+        "todolistID2": [
+            {id: "1", title: "HTML&CSS2", isDone: true},
+            {id: "2", title: "JS2", isDone: true},
+            {id: "3", title: "ReactJS2", isDone: false},
+        ]
+    }
+
+    const action = changeTaskStatusAC("2", false, "todolistID2");
+
+    const endState = tasksReducer(startState, action)
+
+    expect(endState["todolistID2"][1].isDone).toBe(false);
+    expect(endState["todolistID1"][1].isDone).toBe(true);
 })
