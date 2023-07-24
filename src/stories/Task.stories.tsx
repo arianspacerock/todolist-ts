b250c6b8ceda512a44387831a494db091fee1524
arianspacerock <siropemptyspace@gmail.com>
2023-07-24 19:16:24 +0300

    ref: start add stories for the components

--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -1,13 +1,12 @@
 import type {Meta, StoryObj} from '@storybook/react';
-
-import {Button} from './Button';
-import {AddItemForm} from "../AddItemForm";
 import {action} from "@storybook/addon-actions";
+import {Task} from "../Task";
+
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-const meta: Meta<typeof AddItemForm> = {
-    title: 'TODOLISTS/AddItemForm',
-    component: AddItemForm,
+const meta: Meta<typeof Task> = {
+    title: 'TODOLISTS/Task',
+    component: Task,
     parameters: {
         // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
         layout: 'centered',
@@ -15,25 +14,25 @@ const meta: Meta<typeof AddItemForm> = {
     // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
     tags: ['autodocs'],
     // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-    argTypes: {
-        addItem: {
-            description: 'Button clicked inside form',
-            //action: 'clicked'
-        },
+    args: {
+        changeTaskStatus: action('changeTaskStatus'),
+        changeTaskTitle: action('changeTaskTitle'),
+        removeTask: action('removeTask'),
+        task: {id: '1', title: 'JS', isDone: true},
+        todolistId: 'string',
     }
 }
 
 export default meta;
-type Story = StoryObj<typeof AddItemForm>;
+type Story = StoryObj<typeof Task>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const AddItemFormStory: Story = {
-    args: {
-        addItem: action('Button clicked inside form')
-    },
-};
 
-export const AddItemFormErrorStory: Story = {
-    //render
+export const TaskIsDoneStory: Story = {}
+
+export const TaskIsNotDoneStory: Story = {
+    args: {
+        task: {id: '1', title: 'CSS', isDone: false},
+    }
 }
 
