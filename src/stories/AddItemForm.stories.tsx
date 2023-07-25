3904d053cd979f055c8f52c846947a7f4a4edfaf
arianspacerock <siropemptyspace@gmail.com>
2023-07-25 18:13:42 +0300

    ref: start add stories for the components

--- src/stories/AddItemForm.stories.tsx
+++ src/stories/AddItemForm.stories.tsx
@@ -1,9 +1,9 @@
 import type {Meta, StoryObj} from '@storybook/react';
 
 import {Button} from './Button';
-import {AddItemForm} from "../AddItemForm";
+import {AddItemForm, AddItemFormPropsType} from "../AddItemForm";
 import {action} from "@storybook/addon-actions";
-import React, {ChangeEvent, KeyboardEvent, useState} from "react";
+import React, {ChangeEvent, FC, KeyboardEvent, useState} from "react";
 import TextField from "@mui/material/TextField/TextField";
 import {IconButton} from "@mui/material";
 import {AddBox} from "@mui/icons-material";
@@ -37,46 +37,48 @@ export const AddItemFormStory: Story = {
     },
 };
 
-export const AddItemFormErrorStory: Story = {
-    render: (args) => {
-        let [title, setTitle] = useState("")
-        let [error, setError] = useState<string | null>("Title is required")
+const AddItemFormError: FC<AddItemFormPropsType> = (args) => {
+    const [title, setTitle] = useState("")
+    const [error, setError] = useState<string | null>("Title is required")
 
-        const addItem = () => {
-            if (title.trim() !== "") {
-                args.addItem(title);
-                setTitle("");
-            } else {
-                setError("Title is required");
-            }
+    const addItem = () => {
+        if (title.trim() !== "") {
+            args.addItem(title);
+            setTitle("");
+        } else {
+            setError("Title is required");
         }
-
-        const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-            setTitle(e.currentTarget.value)
-        }
-
-        const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-            if (error !== null) {
-                setError(null);
-            }
-            if (e.key === "Enter") {
-                addItem();
-            }
-        }
-
-        return <div>
-            <TextField variant="outlined"
-            error={!!error}
-            value={title}
-            onChange={onChangeHandler}
-            onKeyDown={onKeyPressHandler}
-            label="Title"
-            helperText={error}
-            />
-            <IconButton color="primary" onClick={addItem}>
-                <AddBox />
-            </IconButton>
-        </div>
     }
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (error !== null) {
+            setError(null);
+        }
+        if (e.key === "Enter") {
+            addItem();
+        }
+    }
+
+    return <div>
+        <TextField variant="outlined"
+                   error={!!error}
+                   value={title}
+                   onChange={onChangeHandler}
+                   onKeyDown={onKeyPressHandler}
+                   label="Title"
+                   helperText={error}
+        />
+        <IconButton color="primary" onClick={addItem}>
+            <AddBox />
+        </IconButton>
+    </div>
+}
+
+export const AddItemFormErrorStory: Story = {
+    render: (args) => <AddItemFormError addItem={args.addItem}/>
 }
 
--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -1,6 +1,7 @@
 import type {Meta, StoryObj} from '@storybook/react';
 import {action} from "@storybook/addon-actions";
-import {Task} from "../Task";
+import {Task, TaskPropsType} from "../Task";
+import {FC, useState} from "react";
 
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
@@ -34,5 +35,32 @@ export const TaskIsNotDoneStory: Story = {
     args: {
         task: {id: '1', title: 'CSS', isDone: false},
     }
+}
+
+const TaskWithHook: FC<TaskPropsType> = (args) => {
+    const [task, setTask] = useState(args.task)
+    const changeTaskStatus = () => {
+        setTask({...task, isDone: !task.isDone})
+    }
+    const changeTaskTitle = (title: string) => {
+        setTask({...task, title: title})
+    }
+
+    return <Task changeTaskStatus={args.changeTaskStatus}
+                 changeTaskTitle={args.changeTaskTitle}
+                 removeTask={args.removeTask}
+                 task={task}
+                 todolistId={args.todolistId}
+    />
+}
+
+export const TaskWithHookStory: Story = {
+    render: (args => <TaskWithHook
+        changeTaskStatus={args.changeTaskStatus}
+        changeTaskTitle={args.changeTaskTitle}
+        removeTask={args.removeTask}
+        task={args.task}
+        todolistId={args.todolistId}
+    />)
 }
 
